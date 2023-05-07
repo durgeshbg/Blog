3e43ae146e0fc252a095078d1f9157dba854bb1a
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-05-07 19:21:13 +0530

    add Routes in App.js for Home & Create components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import {BrowserRouter, Routes, Route, useParams} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
 
 function App() {
-    return (
-        <div className='App'>
-            <Navbar />
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
