b71149a2c342b9629aecf56780ea77518eeedf61
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-05-07 19:29:20 +0530

    add imports for components in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route, useParams} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
+import Blog from "./pages/Blog";
+import Update from "./pages/Update";
 
 function App() {
   return (
